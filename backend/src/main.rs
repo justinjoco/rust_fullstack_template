28eac87dbc48526d289bcdce29a066e1e8b2af77
Jutin Joco <justin.joco@gmail.com>
2025-03-06 23:53:18 -0800

    adds initial rust deps and hello world

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, HttpRequest, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder{
+    format!("Hello world!")
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+
+        // Start the Actix Web server
+        HttpServer::new(|| {
+            App::new()
+                .route("/health_check", web::get().to(greet))
+        })
+        .bind("127.0.0.1:5000")?
+        .run()
+        .await
 }
