5448a20d94a0fbc28593b602b5e2a715b4604cee
Jutin Joco <justin.joco@gmail.com>
2025-03-07 14:36:49 -0800

    adds methods to traits

--- backend/src/controller/book_controller.rs
+++ backend/src/controller/book_controller.rs
@@ -1,13 +1,17 @@
 pub trait Controller {
-
+    pub fn list_all_books(&self);
+    pub fn get_book_by_id(&self);
+    pub fn delete_book_by_id(&self);
+    pub fn update_book_by_id(&self);
+    pub fn create_book(&self);
 }
 pub struct BookController{
-
+    
 }
 
 impl BookController{
     pub fn new() -> Self{
-        
+
     }
 }
 
--- backend/src/service/book_service.rs
+++ backend/src/service/book_service.rs
@@ -1,5 +1,9 @@
 pub trait Service{
-
+    pub fn list_all_books(&self);
+    pub fn get_book_by_id(&self);
+    pub fn delete_book_by_id(&self);
+    pub fn update_book_by_id(&self);
+    pub fn create_book(&self);
 }
 
 pub struct BookService{
